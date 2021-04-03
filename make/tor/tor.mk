d6cd870a12d15ee8ae3d21dd1ed9a044dd5501fa
Florian Schilling <flosch.devel@gmail.com>
2021-04-03 21:57:05 +0200

    tor: bump version to 0.4.5.7 Changelog: https://gitweb.torproject.org/tor.git/plain/ChangeLog?h=tor-0.4.5.7

--- make/tor/tor.mk
+++ make/tor/tor.mk
@@ -1,6 +1,6 @@
-$(call PKG_INIT_BIN, 0.4.4.6)
+$(call PKG_INIT_BIN, 0.4.5.7)
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_SOURCE_SHA256:=5f154c155803adf5c89e87cab53017b6908c5ebe50c65839e8cf4fbd2abe1fdc
+$(PKG)_SOURCE_SHA256:=447fcaaa133e2ef22427e98098a60a9c495edf9ff3e0dd13f484b9ad0185f074
 $(PKG)_SITE:=https://www.torproject.org/dist
 ### WEBSITE:=https://www.torproject.org/download/tor/
 ### MANPAGE:=https://trac.torproject.org/projects/tor/wiki/
